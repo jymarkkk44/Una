758ff68f6cdb40c94285c5433ea68c4b9c545304
Your Name <you@example.com>
2025-02-19 22:17:55 +0800

    una

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Students;
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::factory(1000)->create();
+
+        // Student DB
+        Students::create([
+            'name' => 'Christian',
+            'age' => 20,
+        ]);
+        Students::create([
+            'name' => 'Zee',
+            'age' => 22,
+        ]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\StudentsController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/students-list', [StudentsController::class, 'index']);
