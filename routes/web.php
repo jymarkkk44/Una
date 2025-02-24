4df57fcf2dc7df2a266c9a5a508a63b357a39193
Your Name <you@example.com>
2025-02-24 15:16:50 +0800

    Activity 2

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,26 @@
 <?php
+
+use App\Http\Controllers\StudentsController;
+use App\Models\Students;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-use App\Http\Controllers\StudentsController;
-Route::get('/students', [StudentsController::class, 'index']);
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// use App\Http\Controllers\StudentsController;
+// Route::get('/students', [StudentsController::class, 'index']);
 
 
-Route::get('/users', function () {
-    $users = User::all();
-    return view('users', compact('users'));
-});
+// Route::get('/', function () {
+//     $users = User::all();
+//     return view('users', compact('users'));
+// });
+
+// Route::get('/', function () {
+//     $students = Students::all();
+//     return view('studentList', compact('students'));
+// });
+
+Route::get('/', [StudentsController::class, 'index'])->name('std.index');
+Route::post('/create-student', [StudentsController::class, 'newStudent'])->name('std.create');
