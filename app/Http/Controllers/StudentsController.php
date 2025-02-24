13ad1e0cc3ae740bfa6ab5c338fc8b0f127e6824
Your Name <you@example.com>
2025-02-24 15:13:53 +0800

    Activity 2

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -13,4 +13,18 @@ class StudentsController extends Controller
         $students = Students::all();
         return view('studentList', compact('students'));
     }
+
+    public function newStudent(Request $request)
+    {
+        $request->validate([
+            'stdName' => 'required|max:300',
+            'stdAge' => 'required|numeric',
+        ]);
+
+        $input['name'] = $request->stdName;
+        $input['age'] = $request->stdAge;
+        Students::create($input);
+
+        return redirect()->route('std.index')->with('success', 'Student created successfully.');
+    }
 }
